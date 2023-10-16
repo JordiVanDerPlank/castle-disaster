6b9a0d18d110a16bd0c8dbe31308b836b51641b7
Jordi van der Plank <jordivdp1@gmail.com>
2023-10-16 20:11:51 +0200

    Added new camera controls

--- CastleDisaster/Assets/RTS_CameraController.cs
+++ CastleDisaster/Assets/RTS_CameraController.cs
@@ -5,109 +5,45 @@ using UnityEngine.UIElements;
 
 public class RTS_CameraController : MonoBehaviour
 {
+    public float moveSpeed;
+    public float minXRot, maxXRot, curXRot;
+    public float minZoom, maxZoom;
+    public float zoomSpeed, rotateSpeed;
+    private float curZoom;
+    private Camera cam;
 
-    [SerializeField] float minFieldOfView, maxFieldOfView;
-    [SerializeField] float zoomValue, zoomSpeed;
-
-    private void Start()
+    void Start()
     {
-        Camera.main.fieldOfView = zoomValue;
-        rotY = transform.rotation.y;
+        cam = Camera.main;
+        curZoom = cam.transform.localPosition.y;
+        curXRot = -50;
+        cam.transform.localPosition = new Vector3(curXRot, curZoom, 150);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        transform.LookAt(mainCameraCenterPoint);
+        curZoom += Input.GetAxis("Mouse ScrollWheel") * -zoomSpeed;
+        curZoom = Mathf.Clamp(curZoom, minZoom, maxZoom);
+        cam.transform.localPosition = Vector3.up * curZoom;
 
-        zoomValue = Camera.main.fieldOfView;
-
-        float value = Input.GetAxis("Mouse ScrollWheel");
-
-        if (value > 0)  //zoom out
-            zoomValue -= zoomSpeed;
-        if (value < 0)  //zoom in
-            zoomValue += zoomSpeed;
-
-        if (zoomValue < minFieldOfView)
-            zoomValue = minFieldOfView;
-        if (zoomValue > maxFieldOfView)
-            zoomValue = maxFieldOfView;
-
-        Camera.main.fieldOfView = zoomValue;
-
-        AllowRotate();
-        Rotate();
-
-        forward = Camera.main.transform.forward;
-        forward.y = 0;
-        forward = Vector3.Normalize(forward);
-
-        right = Quaternion.Euler(0, 90, 0) * forward;
-
-        GetInput();
-        Move();
-    }
-
-    [SerializeField] Transform mainCameraCenterPoint;
-    [SerializeField] float camRotationSpeed;
-    [SerializeField] bool canRotate;
-
-    [SerializeField] Vector3 currentPos, deltaPos, lastPos;
-
-    void AllowRotate()
-    {
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButton(1))
         {
-            lastPos = currentPos;
-            canRotate = true;
+            float x = Input.GetAxis("Mouse X");
+            float y = Input.GetAxis("Mouse Y");
+            curXRot += -y * rotateSpeed;
+            curXRot = Mathf.Clamp(curXRot, minXRot, maxXRot);
+            transform.eulerAngles = new Vector3(curXRot, transform.eulerAngles.y + (x * rotateSpeed), 0.0f);
         }
 
-        if (Input.GetMouseButtonUp(1))
-            canRotate = false;
-    }
-
-    float rotY;
-    void Rotate()
-    {
-        if (!canRotate)
-            return;
-
-        currentPos = Input.mousePosition;
-        deltaPos = currentPos - lastPos;
-        lastPos = currentPos;
-
-        rotY += deltaPos.x / camRotationSpeed;
-
-        mainCameraCenterPoint.transform.localRotation = Quaternion.Euler(transform.rotation.x, rotY, transform.rotation.z);
-
-    }
-
-    Vector3 forward, right;
-
-    [Header("Player Variables")]
-    [SerializeField] float moveSpeed;
-
-    void GetInput()
-    {
-        h = Input.GetAxis("Horizontal");
-        v = Input.GetAxis("Vertical");
-    }
-
-    float h;
-    float v;
-
-    Vector3 movePos;
-    float distance;
-
-    void Move()
-    {
-        Vector3 direction = new Vector3(h, 0, v);
-
-        Vector3 rightMovement = right * moveSpeed * Time.deltaTime * h;
-        Vector3 upMovement = forward * moveSpeed * 1.5f * Time.deltaTime * v;
-
-        mainCameraCenterPoint.transform.position += rightMovement;
-        mainCameraCenterPoint.transform.position += upMovement;
+        Vector3 forward = cam.transform.forward;
+        forward.y = 0.0f;
+        forward.Normalize();
+        Vector3 right = cam.transform.right.normalized;
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveZ = Input.GetAxisRaw("Vertical");
+        Vector3 dir = forward * moveZ + right * moveX;
+        dir.Normalize();
+        dir *= moveSpeed * Time.deltaTime;
+        transform.position += dir;
     }
 }
